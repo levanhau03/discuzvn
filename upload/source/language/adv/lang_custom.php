b7d80ddec349413397d67a7315d859be704c7394
levanhau03 <nhatki888@gmail.com>
2020-10-13 11:49:34 +0700

    Update lang_custom.php

--- upload/source/language/adv/lang_custom.php
+++ upload/source/language/adv/lang_custom.php
@@ -15,8 +15,8 @@ $lang = array
 (
 	'custom_name' => 'Tùy chọn',
 	'custom_desc' => 'Thêm HTML quảng cáo , các tập tin HTML, có thể được thêm vào bất kỳ trang nào trong trang web quảng cáo. Áp dụng đối với các webmaster biết kiến thức về HTML đơn giản.<br /><br />
-		<a href="javascript:;" onclick="prompt(\'请复制(CTRL+C)以下内容并添加到模板中，添加此广告位\', \'<!--{ad/custom_'.$_GET['customid'].'}-->\')" />内部调用</a>&nbsp;
-		<a href="javascript:;" onclick="prompt(\'请复制(CTRL+C)以下内容并添加到 HTML 文件中，添加此广告位\', \'&lt;script type=\\\'text/javascript\\\' src=\\\''.$_G['siteurl'].'api.php?mod=ad&adid=custom_'.$_GET['customid'].'\\\'&gt;&lt;/script&gt;\')" />外部调用</a>',
+		<a href="javascript:;" onclick="prompt(\'Vui lòng sao chép (CTRL + C) nội dung sau và thêm nó vào mẫu để thêm vùng quảng cáo này\', \'<!--{ad/custom_'.$_GET['customid'].'}-->\')" />Cuộc gọi nội bộ</a>&nbsp;
+		<a href="javascript:;" onclick="prompt(\'Vui lòng sao chép (CTRL + C) nội dung sau và thêm nó vào tệp HTML để thêm vùng quảng cáo này\', \'&lt;script type=\\\'text/javascript\\\' src=\\\''.$_G['siteurl'].'api.php?mod=ad&adid=custom_'.$_GET['customid'].'\\\'&gt;&lt;/script&gt;\')" />Cuộc gọi bên ngoài</a>',
 	'custom_id_notfound' => 'Quảng cáo không tồn tại',
 	'custom_codelink' => 'Code Nội bộ',
 	'custom_text' => 'Mã tùy chọn',
