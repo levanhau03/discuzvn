23ef4b203b45c7c5545334c9b3c76198d7923147
levanhau03 <nhatki888@gmail.com>
2020-10-13 11:05:54 +0700

    Update lang_avatar.php
    100%

--- upload/source/language/task/lang_avatar.php
+++ upload/source/language/task/lang_avatar.php
@@ -17,8 +17,8 @@ $lang = array
 	'avatar_desc' => 'Nhiệm vụ này chỉ áp dụng cho các thành viên chưa tải hình đại diện. Sau khi đăng ký, chỉ cần tải lên hình đại diện để hoàn thành nhiệm vụ và nhận phần thưởng tương ứng',
 	'avatar_view' => '<strong>Thực hiện theo các hướng dẫn để tham gia vào nhiệm vụ này:</strong>
 		<ul>
-		<li>1. <a href="home.php?mod=spacecp&ac=avatar" target="_blank">新窗口打开上传头像页面</a></li>
-		<li>2. 上传一个自己的头像照片</li>
+		<li>1. <a href="home.php?mod=spacecp&ac=avatar" target="_blank">Một cửa sổ mới mở ra trang tải lên hình đại diện</a></li>
+		<li>2. Tải lên ảnh hồ sơ của chính mình</li>
 		</ul>',
 );
 
