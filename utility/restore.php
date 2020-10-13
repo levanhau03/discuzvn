8c6320c3211772ff15e1cd7e4a526abf2404a713
levanhau03 <nhatki888@gmail.com>
2020-10-13 15:27:54 +0700

    Update restore.php

--- utility/restore.php
+++ utility/restore.php
@@ -409,7 +409,7 @@ function show_header() {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=$charset" />
-<title>Discuz! 数据恢复工具</title>
+<title>Discuz! Công cụ khôi phục dữ liệu</title>
 <style type="text/css">
 * { word-break: break-all; }
 body { text-align:center; margin:0; padding:0; background: #F5FBFF; font-size:12px; font-family:Verdana, Arial, Helvetica, 'SimSun', sans-serif; line-height: 1.8; }
@@ -458,8 +458,8 @@ table { border-collapse:collapse; margin-bottom:20px; }
 
 <div class="bodydiv">
 	<h1>
-		Discuz! 数据恢复工具
-		<span> &nbsp; 恢复当中有任何问题请访问技术支持站点 <a href="http://www.discuz.net" target="_blank">http://www.discuz.net</a></span>
+		Discuz! Công cụ khôi phục dữ liệu
+		<span> &nbsp; Nếu bạn có bất kỳ câu hỏi nào trong quá trình khôi phục, vui lòng truy cập trang web hỗ trợ kỹ thuật <a href="http://www.discuz.net" target="_blank">http://www.discuz.net</a></span>
 	</h1>
 	<div class="main">
 EOT;
@@ -489,11 +489,11 @@ function show_msg($message, $url_forward = '', $type = 'message', $success = 0) 
 		echo '<span'.($success ? '' : ' class="red"').'>'.$message.'</span>';
 	} elseif($type == 'redirect') {
 		echo "$message ...";
-		echo "<br /><br /><br /><a href=\"$url_forward\">浏览器会自动跳转页面，无需人工干预。除非当您的浏览器长时间没有自动跳转时，请点击这里</a>";
+		echo "<br /><br /><br /><a href=\"$url_forward\">Trình duyệt sẽ tự động chuyển đến trang mà không cần can thiệp thủ công. Trừ khi trình duyệt của bạn không tự động chuyển hướng trong một thời gian dài, vui lòng nhấp vào đây</a>";
 		echo "<script>setTimeout(\"redirect('$url_forward');\", 1250);</script>";
 	} elseif($type == 'confirm') {
 		echo "$message";
-		echo "<br /><br /><br /><button id=\"confirmbtn\" onclick=\"redirect('$url_forward')\">确定</button><button id=\"cancelbtn\" onclick=\"redirect('{$siteurl}restore.php')\">取消</button>";
+		echo "<br /><br /><br /><button id=\"confirmbtn\" onclick=\"redirect('$url_forward')\">Xác nhận</button><button id=\"cancelbtn\" onclick=\"redirect('{$siteurl}restore.php')\">Hủy bỏ</button>";
 	}
 
 	show_footer();
@@ -543,44 +543,44 @@ function show_tips($tip, $title = '', $comment = '', $style = 1) {
 }
 
 function lang($lang_key, $force = true, $replace = array()) {
-	$lang = array('filename' => '文件名称',
-				'director' => '所在目录',
-				'version' => '版本',
-				'time' => '备份时间',
-				'type' => '类型',
-				'size' => '尺寸',
-				'db_method' => '方式',
-				'db_volume' => '卷数',
-				'import' => '导入',
-				'different_dbcharset_tablepre' => '检测到导入的备份数据与配置文件的{diff} 不同，您还要继续运行此程序吗？',
-				'db_import_tips' => '本功能在恢复备份数据的同时，将全部覆盖原有数据，请确定恢复前已将论坛关闭，恢复全部完成后可以将论坛重新开放。<br />您可以通过数据备份管理功能查看站点的备份文件的详细信息，删除过期的备份,并导入需要的备份。<br /><span class="red">恢复数据的整个过程会在一个新页面完成，您成功恢复数据后请务必及时删除restore.php文件。</span><br />',
-				'db_export_discuz' => 'Discuz! 数据(不含UCenter)',
-				'db_export_discuz_uc' => 'Discuz! 和 UCenter 数据',
-				'db_multivol' => '多卷',
-				'db_import_unzip' => '解压缩',
-				'db_export_zip' => '压缩备份',
+	$lang = array('filename' => 'Tên tập tin',
+				'director' => 'Danh mục',
+				'version' => 'Phiên bản',
+				'time' => 'Thời gian sao lưu',
+				'type' => 'Loại',
+				'size' => 'Kích thước',
+				'db_method' => 'Phương pháp',
+				'db_volume' => 'Số',
+				'import' => 'Nhập khẩu',
+				'different_dbcharset_tablepre' => 'Người ta phát hiện thấy dữ liệu sao lưu đã nhập khác với {diff} của tệp cấu hình. Bạn có muốn tiếp tục chạy chương trình này không?',
+				'db_import_tips' => 'Chức năng này sẽ ghi đè hoàn toàn dữ liệu gốc khi khôi phục dữ liệu sao lưu. Vui lòng đảm bảo rằng diễn đàn đã được đóng trước khi khôi phục và diễn đàn có thể được mở lại sau khi quá trình khôi phục hoàn tất.<br />Bạn có thể xem thông tin chi tiết về các tệp sao lưu của trang web thông qua chức năng quản lý sao lưu dữ liệu, xóa các bản sao lưu đã hết hạn và nhập các bản sao lưu bắt buộc.<br /><span class="red">Toàn bộ quá trình khôi phục dữ liệu sẽ được hoàn tất sang một trang mới, sau khi bạn khôi phục dữ liệu thành công thì hãy xóa kịp thời file restore.php.</span><br />',
+				'db_export_discuz' => 'Discuz! Dữ liệu (không bao gồm UCenter)',
+				'db_export_discuz_uc' => 'Dữ liệu Discuz! Và UCenter',
+				'db_multivol' => 'Dung lượng lớn',
+				'db_import_unzip' => 'Giải nén',
+				'db_export_zip' => 'Sao lưu nén',
 				'db_zip' => 'ZIP',
 				'db_shell' => 'Shell',
-				'unknown' => '未知',
-				'backup_file_unexist' => '备份文件不存在',
-				'connect_error' => '连接数据库失败，请您查看数据库配置文件config/config_global.php和config/config_ucenter.php是否存在以及配置是否正确',
-				'dbcharsetdiff' => ' 数据库字符集($_config[\'db\'][\'1\'][\'dbcharset\'])',
-				'tableprediff' => ' 表前缀($_config[\'db\'][\'1\'][\'tablepre\'])',
-				'database_import_multivol_succeed' => '分卷数据成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
-				'database_import_file_illegal' => '数据文件不存在：可能服务器不允许上传文件或文件大小超过限制',
-				'database_import_multivol_prompt' => '分卷数据第一卷成功导入数据库，您需要自动导入本次备份的其他分卷吗？',
-				'database_import_succeed' => '数据已成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
-				'database_import_format_illegal' => '数据文件非 Discuz! 格式，无法导入',
-				'database_import_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
-				'database_import_multivol_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动解压缩其他的分卷文件吗？',
-				'database_import_multivol_unzip_redirect' => '数据文件 #{multivol} 解压缩成功，程序将自动继续',
-				'database_import_confirm' => '导入和当前 Discuz! 版本不一致的数据极有可能产生无法解决的故障，您确定继续吗？',
-				'database_import_confirm_sql' => '您确定导入该备份吗？',
-				'database_import_confirm_zip' => '您确定解压该备份吗？',
-				'database_import_multivol_confirm' => '所有分卷文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
-				'database_import_multivol_redirect' => '数据文件 #{volume} 成功导入，程序将自动继续',
-				'error_quit_msg' => '必须解决以上问题，才能继续恢复数据',
-				'restored_error' => '恢复数据功能锁定，已经恢复过了，如果您确定要恢复数据，请到服务器上删除./data/restore.lock',
+				'unknown' => 'Không xác định',
+				'backup_file_unexist' => 'Tệp sao lưu không tồn tại',
+				'connect_error' => 'Không kết nối được với cơ sở dữ liệu, vui lòng kiểm tra xem các tệp cấu hình cơ sở dữ liệu config/config_global.php và config/config_ucenter.php có tồn tại hay không và cấu hình có đúng không',
+				'dbcharsetdiff' => ' Bộ ký tự cơ sở dữ liệu($_config[\'db\'][\'1\'][\'dbcharset\'])',
+				'tableprediff' => ' Bảng Prefix($_config[\'db\'][\'1\'][\'tablepre\'])',
+				'database_import_multivol_succeed' => 'Dữ liệu khối lượng phụ được nhập thành công vào cơ sở dữ liệu trang web<br />Vui lòng cập nhật bộ nhớ cache trong nền<br /><span class="red">Vui lòng xóa tệp restore.php càng sớm càng tốt để tránh ảnh hưởng đến dữ liệu</span>',
+				'database_import_file_illegal' => 'Tệp dữ liệu không tồn tại: máy chủ có thể không cho phép tải tệp lên hoặc kích thước tệp vượt quá giới hạn',
+				'database_import_multivol_prompt' => 'Tập dữ liệu con của tập đầu tiên đã được nhập thành công vào cơ sở dữ liệu. Bạn có cần tự động nhập các tập con khác của bản sao lưu này không?',
+				'database_import_succeed' => 'Dữ liệu đã được nhập thành công vào cơ sở dữ liệu trang web<br />Vui lòng cập nhật bộ đệm ẩn trong nền<br /><span class="red">Vui lòng xóa tệp restore.php càng sớm càng tốt để tránh ảnh hưởng đến dữ liệu</span>',
+				'database_import_format_illegal' => 'Tệp dữ liệu không ở định dạng Discuz! Và không thể nhập được',
+				'database_import_unzip' => '{info}<br />Sau khi tệp sao lưu được giải nén, bạn có cần nhập bản sao lưu tự động không? Tệp đã giải nén sẽ bị xóa sau khi nhập',
+				'database_import_multivol_unzip' => '{info}<br />Sau khi tệp sao lưu được giải nén, bạn có cần tự động giải nén các tệp ổ đĩa con khác không?',
+				'database_import_multivol_unzip_redirect' => 'Tệp dữ liệu #{multivol} được giải nén thành công, chương trình sẽ tự động tiếp tục',
+				'database_import_confirm' => 'Việc nhập dữ liệu không phù hợp với phiên bản Discuz! Hiện tại rất có thể gây ra lỗi không thể giải quyết được. Bạn có chắc chắn tiếp tục không?',
+				'database_import_confirm_sql' => 'Bạn có chắc chắn nhập bản sao lưu này không?',
+				'database_import_confirm_zip' => 'Bạn có chắc chắn giải nén bản sao lưu này không?',
+				'database_import_multivol_confirm' => 'Sau khi tất cả các tập tin khối lượng phụ được giải nén, bạn có cần tự động nhập bản sao lưu không? Các tệp được giải nén sau khi nhập sẽ bị xóa',
+				'database_import_multivol_redirect' => 'Đã nhập thành công tệp dữ liệu #{volume}, chương trình sẽ tự động tiếp tục',
+				'error_quit_msg' => 'Các vấn đề trên phải được giải quyết để tiếp tục khôi phục dữ liệu',
+				'restored_error' => 'Chức năng khôi phục dữ liệu đã bị khóa và đã được khôi phục. Nếu bạn chắc chắn khôi phục được dữ liệu, vui lòng xóa ./data/restore.lock trên máy chủ',
 			);
 	$return = isset($lang[$lang_key]) ? $lang[$lang_key] : ($force ? $lang_key : '');
 	if($replace && is_array($replace)) {
